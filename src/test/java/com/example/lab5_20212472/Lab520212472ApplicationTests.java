d7d89122e29557dae96b4069891b48b395a5d1f0
MrFoxito-hack <miguelalvizuri7@gmail.com>
2025-05-16 18:54:04 -0500

    wasa

---
+++ src/test/java/com/example/lab5_20212472/Lab520212472ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20212472;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520212472ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
